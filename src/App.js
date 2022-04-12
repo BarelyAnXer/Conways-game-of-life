1b7f8e368bb46cbf8417146ed0221b4c2fe93dfa
DreamIce-1 <christiangazzingan38@gmail.com>
2022-04-12 18:49:50 +0800

    added randomized colors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 
-const numOfRow = 3
-const numOfCol = 3
+// const numOfRow = Math.round(window.innerWidth / 100)
+// const numOfCol = Math.round(window.innerHeight / 100)
+const numOfRow = 50
+const numOfCol = 50
+const directions = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+]
 
 function App () {
   const [isPaused, setIsPaused] = useState(true)
   const [counter, setCounter] = useState(0)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (!isPaused) {
-        console.log(counter)
-        setCounter(prevState => prevState + 1)
-      }
-    }, 300)
-    return () => clearInterval(interval)
-  }, [isPaused])
 
   const [grid, setGrid] = useState(() => {
     const grid = []
@@ -29,9 +32,66 @@ function App () {
     return grid
   })
 
+  const generateRandomGrid = () => {
+    const grid = []
+    for (let i = 0; i < numOfRow; i++) {
+      const row = []
+      for (let j = 0; j < numOfCol; j++) {
+        row.push(Math.round(Math.random()))
+      }
+      grid.push(row)
+    }
+    return grid
+  }
+
+  function getRandomColor () {
+    let letters = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+  }
+
+  console.log(getRandomColor())
+  // console.log(window.innerWidth)
+  // console.log(window.innerHeight)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPaused) {
+        setCounter(prevState => prevState + 1)
+        setGrid((prevState) => {
+          const gridCopy2 = prevState.map(inner => inner.slice())
+          for (let i = 0; i < numOfRow; i++) {
+            for (let j = 0; j < numOfCol; j++) {
+              let neighbors = 0
+              directions.forEach(([x, y]) => {
+                const newI = i + x
+                const newJ = j + y
+                if (newI >= 0 && newI < numOfRow && newJ >= 0 && newJ <
+                  numOfCol) {
+                  neighbors = neighbors + prevState[newI][newJ]
+                }
+              })
+
+              if (neighbors < 2 || neighbors > 3) {
+                gridCopy2[i][j] = 0
+              } else if (prevState[i][j] === 0 && neighbors === 3) {
+                gridCopy2[i][j] = 1
+              }
+            }
+          }
+
+          return gridCopy2
+        })
+      }
+    }, 100)
+    return () => clearInterval(interval)
+  }, [isPaused])
+
   return (
     <div>
-      <p>{isPaused.toString()}</p>
       <p>generations: {counter}</p>
       <button onClick={() => {
         setIsPaused(prevState => !prevState)
@@ -43,6 +103,12 @@ function App () {
         console.table(grid)
       }}>
         print grid
+      </button>
+
+      <button onClick={() => {
+        setGrid(generateRandomGrid)
+      }}>
+        random
       </button>
 
       <div style={{
@@ -58,25 +124,28 @@ function App () {
                   width: '20px',
                   height: '20px',
                   border: 'solid 1px',
-                  backgroundColor: grid[i][j] === 0 ? 'white' : 'black',
+                  // backgroundColor: grid[i][j] === 0 ? 'white' : 'black',
+                  backgroundColor: grid[i][j] === 0
+                    ? 'white'
+                    : getRandomColor(),
                 }}
                 onClick={() => {
                   // const [grid, setGrid] = useState(some generated 2d array)
-                  const gridCopy = [...grid] // why is this deep copy i feel like it is not lol
+                  // const gridCopy = [...grid] // why is this deep copy i feel like it is not lol maybe wait i think it's not
                   // const gridCopy = JSON.parse(JSON.stringify(grid)) // another example of deep copy
                   // vs const gridCopy = grid // not deep copy
-                  gridCopy[i][j] = gridCopy[i][j] ? 0 : 1
-                  return setGrid(gridCopy)
+                  // gridCopy[i][j] = gridCopy[i][j] ? 0 : 1
+                  // return setGrid(gridCopy)
 
-                  // // another way
-                  // setGrid((prevState) => {
-                  //   const gridCopy = prevState // not deep copy that's why doesn't work
-                  //   const gridCopy = prevState.map((rows) => {
-                  //     return [...rows]
-                  //   })
-                  //   gridCopy[i][j] = gridCopy[i][j] ? 0 : 1
-                  //   return gridCopy
-                  // })
+                  // another way
+                  setGrid((prevState) => {
+                    // const gridCopy = prevState // not deep copy that's why doesn't work
+                    const gridCopy = prevState.map((rows) => {
+                      return [...rows]
+                    })
+                    gridCopy[i][j] = gridCopy [i][j] ? 0 : 1
+                    return gridCopy
+                  })
                 }
                 }>
               </div>
